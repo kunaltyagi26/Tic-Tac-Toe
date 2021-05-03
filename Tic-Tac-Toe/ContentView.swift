9d12be310e2187a908485f7eaea7b85eef197cb6
kunaltyagi26 <kunaltyagi32@gmail.com>
2021-05-03 23:18:59 +0530

    Initial Commit

---
+++ Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Kunal Tyagi on 03/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
